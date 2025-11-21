c616aada2f054ac16e8a7710e676b4bf48637c44
Vivian Li <vivian@vivians-imac-2.rockefeller.edu>
2025-11-21 17:01:52 -0500

    example yaml and cam1 cif

--- colocalization.py
+++ colocalization.py
@@ -6,19 +6,26 @@
 import csv, sys, re
 
 with open ('casII_defense_systems.tsv', 'r') as casII, open ('casIIIA_only_staph_defense_systems.tsv', 'r') as casIII, open('colocalizationlist.txt', 'w') as outfile:
-    casII=csv.reader(casII, delimiter= '\t')
-    print(str(casII))
-    casIII=csv.reader(casIII, delimiter = '\t')
-    print(casIII)
     accession= r"(\w*\.\d)(.*)"
     found=[]
+    headings=next(casII)
+    headings2=next(casIII)
+    casII = csv.reader(casII, delimiter='\t')
+    casIII = csv.reader(casIII, delimiter='\t')
     for system in casII:
-        geneid=re.search(accession, system)
+        gene_id=re.search(accession, system[0]).group(1)
+        if gene_id== 'GCA_019998745.2':
+            print('found it')
         for system2 in casIII:
-            geneid2=re.search(accession, system)
-    if geneid == geneid2:
-        found+=geneid2
+            gene_id2=re.search(accession, system2[0]).group(1)
+            if gene_id2=='GCA_019998745.2':
+                print('found it here too')
+
+        if str(gene_id) == str(gene_id2):
+            found.append(gene_id2)
+            print('compared the two')
 
     for gene in found:
         outfile.write(f'{found}+\n')
 
+#for some reason, it's searching for the system in both files, but it's not comparing the two
